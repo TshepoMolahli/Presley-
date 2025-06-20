c2325aeca0d0b69b8794b14c3457e439acb27a4f
TshepoMolahli <maputomaputo305@gmail.com>
2025-06-20 10:22:47 +0200

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,34 @@
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #f0f4f8;
+  color: #222;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 800px;
+  margin: 50px auto;
+  padding: 0 20px;
+  text-align: center;
+}
+
+h1 {
+  font-size: 3em;
+  color: #1e90ff;
+}
+
+h2 {
+  margin-top: 40px;
+  color: #333;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+a {
+  color: #1e90ff;
+  text-decoration: none;
+}
